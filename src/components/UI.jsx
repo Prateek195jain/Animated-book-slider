bc74a6b5247125648ae9f526371c1ca957745c73
Prateek195jain <118386573+Prateek195jain@users.noreply.github.com>
2025-07-08 01:06:05 +0530

    update: UI.jsx

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -83,52 +83,6 @@ export const UI = () => {
           </div>
         </div>
       </main>
-
-      <div className="fixed inset-0 flex items-center -rotate-2 select-none">
-        <div className="relative">
-          <div className="bg-white/0 animate-horizontal-scroll flex items-center gap-8 w-max px-8">
-            <h1 className="shrink-0 text-white text-10xl font-black">
-              Graphics
-            </h1>
-            <h2 className="shrink-0 text-white text-8xl italic font-light">
-              Animation
-            </h2>
-            <h2 className="shrink-0 text-white text-12xl font-bold">Objects</h2>
-            <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              Camera
-            </h2>
-            <h2 className="shrink-0 text-white text-9xl font-medium">Mesh</h2>
-            <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              Light
-            </h2>
-            <h2 className="shrink-0 text-white text-13xl font-bold">Scene</h2>
-            <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
-              Render
-            </h2>
-          </div>
-
-          <div className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max">
-            <h1 className="shrink-0 text-white text-10xl font-black">
-              Graphics
-            </h1>
-            <h2 className="shrink-0 text-white text-8xl italic font-light">
-              Animation
-            </h2>
-            <h2 className="shrink-0 text-white text-12xl font-bold">Objects</h2>
-            <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              Camera
-            </h2>
-            <h2 className="shrink-0 text-white text-9xl font-medium">Mesh</h2>
-            <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              Light
-            </h2>
-            <h2 className="shrink-0 text-white text-13xl font-bold">Scene</h2>
-            <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
-              Render
-            </h2>
-          </div>
-        </div>
-      </div>
     </>
   );
 };
